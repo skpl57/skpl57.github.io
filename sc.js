3289f29a1263a85a2a8e870930b22108db3025ee
skpl57 <szymon.kania@onet.com.pl>
2024-04-23 19:03:54 +0200

    1.1

--- sc.js
+++ sc.js
@@ -1,20 +1,22 @@
 $(document).ready(
-    //41253
+    //24153
     function(){
         let przycisk=false
         let guzik=false
         let knefel=false
         let button=false
         let key=false
+        let woda=false
+        let waz=false
         $("#g1,#g2,#g3,#g4,#g5").click(
             function(){
                 $("#kod").fadeOut(0);
             }
         );
-        $("#g1").click(
+        $("#g2").click(
             function(){
                 if (przycisk==false && guzik==false && knefel==false && button==false && key==false){
-                    $("#g1").fadeOut(200);
+                    $("#g2").fadeOut(200);
                     button=true
                 }
                 else{
@@ -27,10 +29,10 @@ $(document).ready(
                 }
             }
         );
-        $("#g2").click(
+        $("#g1").click(
             function(){
                 if (przycisk==true && guzik==false && knefel==false && button==true && key==false){
-                    $("#g2").fadeOut(200);
+                    $("#g1").fadeOut(200);
                     guzik=true
                 }
                 else{
@@ -96,28 +98,36 @@ $(document).ready(
             function(){
                 $("#klapa").addClass("animacja");
                 $("#g1").delay(1000).animate({"top":"0px"},800);
+                $("#klapa").delay(3000).fadeOut(500)
             }
         );
         $("#strzala").click(
             function(){
-                $(this).addClass("strz");
-                $("#g4").delay(5500).animate({"top":"0"},400);
+                $(this).addClass("strz").delay(5100).fadeOut(0);
+                $("#g4").delay(5500).animate({"top":"0"},800);
+                $("#dis").delay(6000).fadeOut(500)
             }
         );
-        let waz=false
         $("#waz").click(
             function(){
-                $(this).animate({"top":"80%", "left":"8%"},2000);
-                waz=true
+                if(woda==false){
+                    $(this).animate({"top":"80%", "left":"8%"},2000);
+                    waz=true
+                    woda=true
+                }
+                    else{
+                        $(this).delay(200)
+                    }
             }
         );
         $("#kran").click(
             function(){
                 if(waz==true){
                     $("#puste").fadeOut(2000);
-                    $("#waz").delay(2500).animate({"top":"110%","left":"0%"},2000)
+                    $("#waz").delay(2500).animate({"top":"110%","left":"0%"},2000).fadeOut(1000);
+                    $("#kran").delay(4500).fadeOut(1000);
                 }
-
+                    else{}
             }
         );
     }
